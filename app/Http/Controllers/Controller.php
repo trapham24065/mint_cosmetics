b862fd950910c5a13cc874e5b06a13479c0dfc11
vandunxg <nguyenvandung.official@gmail.com>
2025-08-20 22:56:18 +0700

    initial project

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    //
+    public function index()
+    {
+        // This method can be overridden in child controllers
+    }
+}
