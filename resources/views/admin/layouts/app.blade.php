44edd26164291ef37bbeca6db8c2eae0274de8b5
vandunxg <nguyenvandung.official@gmail.com>
2025-08-22 15:41:27 +0700

    fix: fix asset path

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -12,12 +12,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{asset('template/admin/assets/images/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
 
     <!-- App css -->
-    <link href="{{asset('template/admin/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('template/admin/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('template/admin/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
 
 </head>
 
@@ -33,13 +33,13 @@
 @include('admin.layouts.footer')
 <!-- vendor js -->
 @vite(['resources/css/app.css', 'resources/js/app.js'])
-<script src="{{asset('template/admin/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('template/admin/assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
 
-<script src="{{asset('template/admin/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
-<script src="{{asset('template/admin/assets/js/pages/index.init.js')}}"></script>
-<script src="{{asset('template/admin/assets/js/DynamicSelect.js')}}"></script>
-<script src="{{asset('template/admin/assets/js/app.js')}}"></script>
+<script src="{{asset('assets/libs/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{asset('assets/js/pages/index.init.js')}}"></script>
+<script src="{{asset('assets/js/DynamicSelect.js')}}"></script>
+<script src="{{asset('assets/js/app.js')}}"></script>
 </body>
 
 </html>
