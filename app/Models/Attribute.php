567c9b97224366b8444dc6b059f1b00e3c5182b5
phamtra12 <trapham24065@gmail.com>
2025-08-22 15:34:22 +0700

    create : model , enum , traits

---
+++ app/Models/Attribute.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @project mint_cosmetics
+ * @author PhamTra
+ * @email trapham24065@gmail.com
+ * @date 8/22/2025
+ * @time 3:24 PM
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Attribute extends Model
+{
+
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    /**
+     *
+     * An attribute has multiple values.
+     */
+    public function values(): HasMany
+    {
+        return $this->hasMany(AttributeValue::class);
+    }
+
+    /**
+     * An attribute can belong to multiple categories.
+     */
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'attribute_category');
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class, 'attribute_product');
+    }
+
+}
---
+++ database/factories/CartFactory.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @project mint_cosmetics
+ * @author PhamTra
+ * @email trapham24065@gmail.com
+ * @date 8/22/2025
+ * @time 3:27 PM
+ */
+
+namespace Database\Factories;
+
+use App\Models\Cart;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CartFactory extends Factory
+{
+
+    protected $model = Cart::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+
+}
