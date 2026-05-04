29b9d36c7fed8f7ba3d7d7f3fd447b912f04a588
phamtra12 <trapham24065@gmail.com>
2026-05-04 15:13:39 +0700

    fix bug unique index to guest session

--- resources/views/storefront/product-detail.blade.php
+++ resources/views/storefront/product-detail.blade.php
@@ -5,7 +5,7 @@
     <section class="page-header-area pt-10 pb-9" data-bg-color="#FFF3DA">
         <div class="container">
             <div class="row">
-                <div class="col-md-5">
+                <div class="col-md-12">
                     <div class="page-header-st3-content text-center text-md-start">
                         <ol class="breadcrumb justify-content-center justify-content-md-start">
                             <li class="breadcrumb-item"><a class="text-dark" href="{{ route('home') }}">Trang
